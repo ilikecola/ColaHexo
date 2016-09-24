fee48e2ec76dae19da831518d955fbf1b88787a2
ilikecola <knight.lost.heaven@gmail.com>
2016-09-25 02:52:24 +0800

    Site updated: 2016-09-25 02:52:24

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  $('a.blog-button').click(function() {    
+  $('a.home-button').click(function() {    
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) {
     	$('.panel-cover').removeClass('animated panel-cover--collapsed slideInLeft');
     	$('.content-wrapper').removeClass('animated slideInLeft');
@@ -14,7 +14,27 @@ $(document).ready(function() {
     	$('.content-wrapper').addClass('animated slideInLeft');
     }    
   });
+  
+  $('a.blog-button').click(function() {
+    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
+    $('.panel-cover').removeClass('animated slideInRight');
+    $('.content-wrapper').removeClass('animated slideInRight');
+    $('.panel-cover').addClass('animated panel-cover--collapsed slideInLeft');
+    $('.content-wrapper').addClass('animated slideInLeft');
+  });
 
+	if (window.location.hash && window.location.hash == "#blog") {
+    $('.panel-cover').addClass('panel-cover--collapsed');
+  }
+
+	if (window.location.hash && window.location.hash == "about") {
+    $('.panel-cover').addClass('panel-cover--collapsed');
+  }
+
+	if (window.location.hash && window.location.hash == "archive") {
+    $('.panel-cover').addClass('panel-cover--collapsed');
+  }
+  
   if (window.location.pathname != "/") {       // if hexo in subdir of site, should change this line
     $('.panel-cover').addClass('panel-cover--collapsed');
   }
